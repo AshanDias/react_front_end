e30ca9877c95abcfaf860654dd8c17d2cd5078e9
ashan <ashandias.info@gmail.com>
2021-05-11 16:00:52 +0530

    Added form

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import AdminProduct from './admin/product/view/View'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import AdminProducts from "./admin/product/view/View";
+import CreateProducts from "./admin/product/create/Create"
 function App() {
   return (<Router>
     <div className="App">
       
       <Switch>
-        <Route path="/admin/product" exact component={AdminProduct}></Route> 
+        <Route path="/admin/product" exact component={AdminProduct}></Route>
+        <Route path="/admin/product/create" exact component={CreateProducts}></Route>  
       </Switch>
     </div>
   </Router>
---
+++ src/admin/product/create/Create.js
@@ -0,0 +1,78 @@
+import React, { Component } from "react";
+class CreateProducts extends Component {
+
+    constructor(props) {
+        super(props);
+
+     
+        this.state = {
+          name: "",
+          desc: "",
+          image: "",
+          price: null
+        };
+
+        this.myChangeHandler = this.myChangeHandler.bind(this)
+        this.submitForm = this.submitForm.bind(this)
+      }
+
+        myChangeHandler = (event) => {
+        let nam = event.target.name;
+        let val = event.target.value;
+        this.setState({[nam]: val});
+      }
+    render(){
+        return (
+          
+<div className="container-fluid">
+	<div className="row">
+    <div className="col-md-4"></div>
+		<div className="col-md-4">
+			<form>
+				<div className="form-group">
+					 
+					<label>
+						Name
+					</label>
+					<input type="text" name="name" className="form-control"  onChange={this.myChangeHandler} />
+				</div>
+				<div className="form-group">
+					 
+					<label>
+						Description
+					</label>
+					<input type="text" name="desc"  className="form-control" onChange={this.myChangeHandler} />
+				</div>
+                <div className="form-group">
+					 
+					<label>
+						Image
+					</label>
+					<input type="text" name="image" className="form-control" onChange={this.myChangeHandler} />
+				</div>
+
+                <div className="form-group">
+					 
+					<label>
+						Price
+					</label>
+					<input type="number" name="price" value={this.state.value} className="form-control" onChange={this.myChangeHandler} />
+				</div>
+			
+				
+				<button type="button" className="btn btn-primary" onClick={this.submitForm}>
+					Submit
+				</button>
+			</form>
+		</div>
+     </div>
+</div>
+        )
+    }
+
+    submitForm(){
+        console.log(this.state)
+    }
+}
+
+export default CreateProducts;
