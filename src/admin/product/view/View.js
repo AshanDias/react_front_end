d3b55bcc7e7501b665856c133dfabe97cbe58321
ashan <ashandias.info@gmail.com>
2021-05-12 12:02:19 +0530

    Add structured table

--- src/admin/product/view/View.js
+++ src/admin/product/view/View.js
@@ -1,23 +1,35 @@
 
 import React, { Component } from "react";
 import Parser from 'html-react-parser';
-
+import axios from "axios";
+import swal from 'sweetalert';
 import { AxiosProvider, Request, Get, Delete, Head, Post, Put, Patch, withAxios } from 'react-axios'
-
+import Items from './item'
 
 class AdminProducts extends Component {
 
-
+    state = {
+        proucts: [],
+    };
     constructor(props) {
         super(props);
-
-        this.EditData = this.EditData.bind(this)
 
       }
 
 
-    render(){
+     render(){
         var result =null
+       console.log(this.state)
+        // let items = data.map((item) => {
+        //   return {
+        //     _id: item._id,
+        //     desc:item.desc,
+        //     image:item.image,
+        //     price:item.price
+        //   };
+        // });
+        // this.setState({ proucts: items });
+
         return (
             
             <div>
@@ -39,7 +51,38 @@ class AdminProducts extends Component {
         
 
             <br></br>
-        <Get url="http://localhost:4000/api/admin/products/" params={{}}>
+
+            <table className="table">
+                  
+                  <thead>
+                        <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Name</th>
+                        <th scope="col"> Description</th>
+                        <th scope="col">Image URL</th>
+                        <th scope="col">Price</th>
+                        </tr>
+                </thead>
+                <tbody>
+                {
+
+                this.state.proucts.map((item) => (
+
+                  <Items 
+                    product={item}
+                  />
+
+                ))
+                
+                }
+
+              
+                </tbody>
+              
+              </table>
+
+            
+        {/* <Get url="http://localhost:4000/api/admin/products/" params={{}}>
           {(error, response, isLoading, makeRequest, axios) => {
             if(error) {
               return (<div>Something bad happened: {error.message} <button onClick={() => makeRequest({ params: { reload: true } })}>Retry</button></div>)
@@ -49,21 +92,29 @@ class AdminProducts extends Component {
             }
             else if(response !== null) {
                var html = ""
+                console.log("aaa")
 
+                let items = response.data.map((item) => {
+                    return {
+                      _id: item._id,
+                     
+                    };
+                  });
 
-                 result = response.data
-                result.forEach(element => {
+            //    this.setState({proucts: items})
+                //  result = response.data
+                // result.forEach(element => {
                    
-                    html += '<tr>'+
-                    '<td scope="row">'+element["_id"]+'</td>'+
-                    '<td scope="row">'+element["name"]+'</td>'+
-                    '<td scope="row">'+element["desc"]+'</td>'+
-                    '<td scope="row">'+element["image"]+'</td>'+
-                    '<td scope="row">'+element["price"]+'</td>'+
-                    '</tr>'
+                //     html += '<tr>'+
+                //     '<td scope="row">'+element["_id"]+'</td>'+
+                //     '<td scope="row">'+element["name"]+'</td>'+
+                //     '<td scope="row">'+element["desc"]+'</td>'+
+                //     '<td scope="row">'+element["image"]+'</td>'+
+                //     '<td scope="row">'+element["price"]+'</td>'+
+                //     '</tr>'
 
-                });
-              
+                // });
+                console.log(this.state.proucts)
               return (
               
               <div>
@@ -97,13 +148,31 @@ class AdminProducts extends Component {
             return (<div>Default message before request is made.</div>)
           }}
         </Get>
+     */}
+    
       </div>
         );
     }
 
-    EditData(){
-       console.log("tap")
-    }
+  async componentDidMount() {
+    await axios({
+      method:'GET',
+      url: 'http://localhost:4000/api/admin/products',
+      headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Content-Type": "application/json",
+          Accept: "application/json"
+        },
+     
+      
+  }).then(response => {
+      if (response && response.data) {
+        this.setState({ proucts: response.data });
+      }
+    })
+    .catch(error =>  swal("Error!", "An Error Occured!", "error"));
+  }
+  
 
 }
 
---
+++ src/admin/product/view/item.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class Items extends Component {
+    render() {
+
+        return (
+            <tr>
+                <td scope="col">{this.props.product._id}</td>
+                <td scope="col">{this.props.product.name}</td>
+                <td scope="col">{this.props.product.desc}</td>
+                <td scope="col">{this.props.product.image}</td>
+                <td scope="col">{this.props.product.price}</td>
+            </tr>
+        )
+    }
+
+}
+export default Items;
